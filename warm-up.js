c38e12084aff31a39822cd15b34fc926da1e60ce
maryfranks <maryfranks@gmail.com>
2017-08-12 13:59:58 -0400

    warm up conversions

---
+++ warm-up.js
@@ -0,0 +1,41 @@
+// for (var i = 1; i < 11; i++) {
+//   console.log("Doing the same thing over and over.")
+// }
+//
+// var base = 5
+// for (var i = 1; i < 21; i++) {
+//   console.log(i + base);
+// }
+//
+// total = 0
+// for (var i = 1; i < 101; i++) {
+//   total = i;
+// }
+// console.log(total);
+//
+// for (var i = 3; i < 15; i++) {
+//   if (i > 9) {
+//     console.log("You can get on the rollercoaster!")
+//   } else {
+//     console.log("You are too short to ride this rollercoaster.")
+//   }
+// }
+//
+// var containers = ['purse', 'wallet', 'backback']
+// for (var i = 0; i < containers.length; i++) {
+//   var container = containers[i];
+//   console.log(container);
+// }
+
+// function hello_world () {
+//   console.log("Hello world!");
+// }
+//
+// hello_world()
+
+function add(first_num, second_num) {
+  return first_num + second_num
+}
+
+amount = add(5, 7);
+console.log(amount);
